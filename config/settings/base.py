a6ebc85f0f07bf030632a2cd755dd1b9ef9c7da4
Nikolay <nikolay.p.95.workmail@gmail.com>
2026-05-27 03:53:15 +0500

    Обновлен .env.example Добавлен .dockerignore

--- config/settings/base.py
+++ config/settings/base.py
@@ -85,7 +85,7 @@ TEMPLATES = [
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_NAME'),
+        'NAME': os.getenv('POSTGRES_DB'),
         'USER': os.getenv('POSTGRES_USER'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
         'HOST': os.getenv('POSTGRES_HOST'),
